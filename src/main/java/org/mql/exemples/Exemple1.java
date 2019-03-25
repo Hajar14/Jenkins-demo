424f886e04de1759f5e5558aab5e883156a69c40
hajar <hajar@gmail.com>
2019-03-25 21:37:55 +0100

    premiere modification

--- src/main/java/org/mql/exemples/Exemple1.java
+++ src/main/java/org/mql/exemples/Exemple1.java
@@ -4,7 +4,7 @@ public class Exemple1 {
 
 	public static void main(String[] args) {
 		System.out.println("notre premier exemple jenkins");
-
+		System.out.println("notre premiere modification ");
 	}
 
 }
